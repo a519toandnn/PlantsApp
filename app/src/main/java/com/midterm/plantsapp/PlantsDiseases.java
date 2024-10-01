f4288044f3875c58f7e62c909394723a389c041e
a519toandnn <dangngoctoannthst@gmail.com>
2024-10-01 08:36:02 +0700

    first commit

---
+++ app/src/main/java/com/midterm/plantsapp/PlantsDiseases.java
@@ -0,0 +1,30 @@
+package com.midterm.plantsapp;
+
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class PlantsDiseases extends AppCompatActivity {
+
+    private ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_plants_diseases);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.plants_diseases_main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+        btnBack = findViewById(R.id.btn_back);
+        btnBack.setOnClickListener(view -> finish());
+    }
+}
